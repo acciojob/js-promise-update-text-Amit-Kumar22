6084eec013490fd75a626da0b82e3360dcbb5631
Amit-Kumar22 <135856138+Amit-Kumar22@users.noreply.github.com>
2023-09-04 21:56:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+const output=document.getElementById("output");
+	let prom=new Promise((resolve)=>{
+		setTimeout(()=>{
+		resolve("Hello, world!")
+		})
+	})
+prom.then((data)=>{
+	output.innerText=data
+})
